649f7252ce699ce1b4f5962018bd62fb816ebba8
David Chiu <david20571015@gmail.com>
2023-10-19 02:48:06 -0500

    feat: add `ChromaDBWriter` (#1)
    * feat: chroma db writer
    
    * doc: docstring for `ChromaDBWriter`

---
+++ sync_crawler/writer/chroma_writer.py
@@ -0,0 +1,43 @@
+from collections.abc import Iterable
+
+import chromadb
+from chromadb.utils import embedding_functions
+from typing_extensions import override
+
+from proto import news_pb2
+from sync_crawler.writer.base_writer import BaseWriter
+
+
+class ChromaDBWriter(BaseWriter):
+
+    def __init__(
+        self,
+        host: str = 'localhost',
+        port: str = '8000',
+        collection_name: str = 'news',
+        embedding_function_name: str = 'distiluse-base-multilingual-cased-v1',
+    ):
+        """Initialize ChromaDBWriter.
+
+        Args:
+            host: Host of ChromaDB server.
+            port: Port of ChromaDB server.
+            collection_name: Name of collection.
+            embedding_function_name: Name of embedding model. All available
+                models can be found [here](https://www.sbert.net/docs/pretrained_models.html)
+
+        """
+        self._client = chromadb.HttpClient(host=host, port=port)
+        self._embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(
+            model_name=embedding_function_name, device=None)
+
+        self._collection = self._client.get_or_create_collection(
+            collection_name, embedding_function=self._embedding_function)
+
+    @override
+    def put(self, _ids, messages: Iterable[news_pb2.News]):  # pylint: disable=no-member
+        _ids = list(map(str, _ids))
+        documents = list(map(lambda msg: ' '.join(msg.content), messages))
+        embeddings = self._embedding_function(documents)
+
+        self._collection.add(ids=_ids, embeddings=embeddings)
