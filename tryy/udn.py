72282724a5793a8ea174c9ca1fa2769dd8ca36bd
Ciao <c.c.candy21@gmail.com>
2023-12-25 14:15:05 +0800

    12/25 success

--- tryy/udn.py
+++ tryy/udn.py
@@ -1,8 +1,11 @@
 # -!- coding: utf-8 -!-
 
-from test.base_class import Base
+from tryy.base_class import Base
+import re
 
 def udn_crawler(size=10):
+
+    print("in udn")
 
     media = '聯合'
     headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"}
@@ -11,37 +14,53 @@ def udn_crawler(size=10):
 
     link = 'https://tfc-taiwan.org.tw/articles/report'
     urls = []
-    soup = Base.get_page(link)
-    sel = soup.find_all('div', class_='view-content')
 
+    temp_base = Base(title=None, content=None, category=None, modified_date=None, media=None)
+    
+    soup = temp_base.get_page(link, headers)
+    sel = soup.find('div', class_='view-content')
+    sel = soup.find_all('h3', class_='entity-list-title')
+    
     for s in sel:
-        u = s.find('a')['href']
+        temp = s.find('a')
+        u = temp.get('href')
         urls.append('https://tfc-taiwan.org.tw' + u)
+    # print(urls)
 
     article_count = 0
     for url in urls:
         instance = Base("Title", "Content", "Category", "Modified_Date", "聯合", url=url, headers=headers)
         try:
-            soup = instance.get_page(instance.url, headers)
+            # print(url)
+            soup = instance.url
+            # print("success__getting__page")
 
-            title_selector = 'article-content__title'
+            title_selector = 'h2.node-title'
             title = instance.get_title(soup, title_selector)
+            # print("success__getting__title: ", title)
 
-            modified_date = soup.find(class_='article-content__time').text
+            modified_date = soup.select_one('div.submitted')
+            modified_date = modified_date.text
             modified_date = instance.get_modified_date(modified_date)
+            # print("date: ", modified_date)
 
-            # category = soup.find_all(class_='breadcrumb-items')[1].text
-            category_selector = '.breadcrumb-items'
-            category = instance.get_category(soup, category_selector)[1].text.strip()
+            category = []
+            category_select = soup.find("div", class_="node-tags")
+            a_elements = category_select.find_all("a")
+            for a in a_elements:
+                if (a.text != '事實查核報告'):
+                    category.append(a.text)
+            # print("success__getting__category: ", category)
 
             tags = []
 
-            # article_body = soup.find(class_='article-content__editor').find_all('p')
-            content_selector = '.article-content__editor p'
-            content = instance.get_content(soup, content_selector, title)
+            content = soup.find("div", class_="node-content")
+            content_selector = 'p:lang(zh)'
+            content = instance.get_content(content, content_selector, title)
+            # print("success__getting__content: ", content)
 
-            url_hash = instance.generate_hash(instance.url)
-            content_hash = instance.generate_hash(content)
+            # print("success__getting__url_hash: ", instance.url_hash)
+            # print("success__getting__cont_hash: ", instance.content_hash)
 
             news_dict = {
                 'title': title,
@@ -51,8 +70,8 @@ def udn_crawler(size=10):
                 'media': media,
                 'tags': tags,
                 'url': url,
-                'url_hash': url_hash,
-                'content_hash': content_hash
+                'url_hash': instance.url_hash,
+                'content_hash': instance.content_hash
             }
 
             # print(news_dict)
@@ -70,3 +89,5 @@ def udn_crawler(size=10):
             continue
 
     return article_list
+
+# result = udn_crawler(size=1)
