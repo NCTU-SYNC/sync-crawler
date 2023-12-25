6a4ef55f0429446dc59fefa308e4adfcf5f9fdf6
Ciao <c.c.candy21@gmail.com>
2023-12-25 19:54:59 +0800

    12/25 success

--- tryy/cts.py
+++ tryy/cts.py
@@ -1,6 +1,6 @@
 # -!- coding: utf-8 -!-
 
-from base_class import Base
+from tryy.base_class import Base
 
 def cts_crawler(size=30):
 
@@ -9,57 +9,51 @@ def cts_crawler(size=30):
 
     article_list = []
 
-    urls = []
-    try:
-        soup = Base.get_page('https://news.cts.com.tw/real/index.html')
-        newslist_str = "div.newslist-container"
-        list_container = soup.select(newslist_str)
-        a_list = list_container[0].find_all('a', href=True)
-        for a_tag in a_list:
-            urls.append(a_tag['href'])
+    temp_base = Base(title=None, content=None, category=None, modified_date=None, media=None)
+    soup = temp_base.get_page('https://news.cts.com.tw/real/index.html', headers)
+    sel = soup.select('div.newslist-container')
+    sel = sel[0].find_all('a', href=True)
 
-    except Exception as e:
-        print("cts")
-        print("url list fetch error")
-        print(e)
+    urls = []
+    for s in sel:
+        urls.append(s['href'])
+    # print(urls)
 
     news_count = 0
     for url in urls:
         instance = Base("Title", "Content", "Category", "Modified_Date", "華視", url=url, headers=headers)
         try:
-            # soup = get_page(url)
-            soup = instance.get_page(instance.url, headers)
+            # print(url)
+            soup = instance.url
+            # print("success__getting__page")
 
-            # title = soup.select("h1.artical-title")[0].text
             title_selector = 'h1.artical-title'
             title = instance.get_title(instance.url, title_selector)
+            # print("success__getting__title: ", title)
 
-            # date_text = soup.select("time.artical-time")[0].text
-            # modified_date = datetime.datetime.strptime(date_text, "%Y/%m/%d %H:%M")
-            # modified_date = utilities.convert_to_utc(modified_date)
-            modified_date = soup.select("time.artical-time")[0].text
+            modified_date = soup.select("time.artical-time")
+            modified_date = modified_date[0].text
             modified_date = instance.get_modified_date(modified_date)
+            # print("success__getting__modified_date: ", modified_date)
 
-            # category = soup.select('div.item.menu-active')[-1].text
             category_selector = 'div.item.menu-active'
             category = instance.get_category(soup, category_selector)[-1].text
+            # print("success__getting__category: ", category)
 
-            # tags = []            
-            # for a in soup.select("div.news-tag")[0].find_all('a'):
-            #     tags.append(a.text)
             tags = []
             tags_span = soup.select("div.news-tag")[0].find_all('a')
             for tag in tags_span:
                 tags.append(tag.text)
+            # print("success__getting__tags: ", tags)
 
             content_selector = 'div.artical-content'
             content = instance.get_content(soup, content_selector, title)
+            # print("success__getting__content: ", content)
 
-            url_hash = instance.generate_hash(instance.url)
-            content_hash = instance.generate_hash(content)
+            # print("success__getting__url_hash: ", instance.url_hash)
+            # print("success__getting__cont_hash: ", instance.content_hash, "\n\n")
 
             news_dict = {}
-
             news_dict['title'] = title
             news_dict['content'] = content
             news_dict['category'] = category
@@ -67,8 +61,8 @@ def cts_crawler(size=30):
             news_dict['media'] = media
             news_dict['tags'] = tags
             news_dict['url'] = url
-            news_dict['url_hash'] = url_hash
-            news_dict['content_hash'] = content_hash
+            news_dict['url_hash'] = instance.url_hash
+            news_dict['content_hash'] = instance.content_hash
 
             # print(news_dict)
             article_list.append(news_dict)
@@ -84,3 +78,5 @@ def cts_crawler(size=30):
             continue
 
     return article_list
+
+# result = cts_crawler(size=1)
