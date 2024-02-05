235fd194e1cffe145d55b0b10bad82ad326adfc9
Ciao <c.c.candy21@gmail.com>
2024-02-05 21:06:56 +0800

    2/5

---
+++ tryy/cna_class.py
@@ -0,0 +1,38 @@
+from proto.news_pb2 import News
+from tryy.base_class import BaseCrawler
+
+
+class cnaCrawler(BaseCrawler):
+
+    def __init__(self, custom_property, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def cna_urls(self, headers):
+        url = 'https://www.cna.com.tw/list/aall.aspx'
+        soup = super().get_page(url, headers)
+        sel = soup.find('ul', 'mainList imgModule',
+                        id='jsMainList').find_all('li')
+        urls = [s.find('a')['href'] for s in sel if s.find('a')]
+        return urls
+
+    def cna_modified_date(self, soup):
+        modified_date = soup.find('div', class_='updatetime').text
+        modified_date = super().get_modified_date(modified_date)
+        return modified_date
+
+    def cna_category(self, soup):
+        category_selector = 'div.breadcrumb a'
+        category = super().get_category(soup, category_selector)
+        category = category[1].text
+        return category
+
+    def cna_tag(self, soup):
+        tag_links = soup.select('div.keywordTag a')
+        tags = [tag.get_text().replace('#', '') for tag in tag_links]
+        return tags
+
+    def cna_content(self, soup, title):
+        content = soup.find("div", class_="paragraph")
+        content_selector = 'p:lang(zh)'
+        content = super().get_content(content, content_selector, title)
+        return content
