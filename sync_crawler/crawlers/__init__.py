ee2a9991cb9fa46e20a963577b2996c4fbe4b9e4
Flian <flian@inncom.cloud>
2024-09-16 21:15:53 +0800

    feat: cts crawler

--- sync_crawler/crawlers/__init__.py
+++ sync_crawler/crawlers/__init__.py
@@ -1,4 +1,5 @@
 """Crawlers for crawling data from different news websites."""
 
 from .base_crawler import BaseCrawler as BaseCrawler
+from .cts_crawler import CtsCrawler as CtsCrawler
 from .ltn_crawler import LtnCrawler as LtnCrawler
