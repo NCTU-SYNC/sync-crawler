c8fe5667d63fcaff031ef311932c24ebcb7531c1
FlianLiu <eric20020226@gmail.com>
2024-10-06 22:47:09 +0800

    feat: ettoday crawler & adjust the return URL for factcheckcenter news

--- sync_crawler/crawlers/ettoday_crawler.py
+++ sync_crawler/crawlers/ettoday_crawler.py
@@ -14,15 +14,15 @@ from sync_crawler.models.news import News
 
 
 @dataclass
-class FactcheckcenterNewsMetadata:
+class EttodayNewsMetadata:
     publish_time: datetime
     category: Optional[str]
     url: str
 
 
-class FactcheckcenterCrawler(BaseCrawler):
-    media_name = "factcheckcenter"
-    metadata_api = "https://tfc-taiwan.org.tw"
+class EttodayCrawler(BaseCrawler):
+    media_name = "ettoday"
+    metadata_api = "https://www.ettoday.net"
 
     def read(self, start_from: datetime) -> Iterable[News]:
         for page in count(0, step=1):
@@ -34,9 +34,13 @@ class FactcheckcenterCrawler(BaseCrawler):
                     f'Stop crawling, because of {type(e).__name__}: "{e}"'
                 )
 
-            if not metadatas or metadatas[0].publish_time < start_from:
+            if (
+                not isinstance(metadatas, list)
+                or metadatas[0].publish_time < start_from
+            ):
                 self.logger.info("Finish crawling.")
                 return
+
             with ThreadPoolExecutor() as executor:
                 metadatas = filter(lambda x: x.publish_time >= start_from, metadatas)
 
@@ -54,28 +58,33 @@ class FactcheckcenterCrawler(BaseCrawler):
                     else:
                         yield news
 
-    def _crawl_news(self, metadata: FactcheckcenterNewsMetadata) -> News:
-        response = requests.get(metadata.url, allow_redirects=False)
+    def _crawl_news(self, metadata: EttodayNewsMetadata) -> News:
+        response = requests.get(
+            metadata.url,
+            allow_redirects=False,
+        )
         response.raise_for_status()
         response.encoding = "utf-8"
 
         soup = bs4.BeautifulSoup(response.text, "html.parser")
-        html = soup.select("div.node-inner")[0]
+        html = soup.select("div.story")
+
         if not html:
             raise ValueError("Article element not found.")
 
-        title_elements = html.find("h2", {"class": "node-title"})
-        if not title_elements:
+        title_element = soup.select("header > h1.title")
+        if not title_element:
+            title_element = soup.select("header > h1.title_article")
+        if not title_element:
             raise ValueError("Title element not found.")
+        title = title_element[0].string.strip()
 
-        title = title_elements.string.strip()
-        content = soup.select("div.node-preface > p")  # preface
-        content = content + soup.select(
-            "div.field-item > h2,\
-            div.field-item > p > span:not(:has(*)),\
-            div.field-item > p:not(:has(*))"
-        )
-        content = list(map(lambda c: c.string.strip(), content))
+        content = soup.select("div.story > p:not(:has(img))")
+        content = [
+            c.string.strip()
+            for c in content
+            if isinstance(c, bs4.element.Tag) and c.string
+        ]
 
         return News(
             title=title,
@@ -89,33 +98,51 @@ class FactcheckcenterCrawler(BaseCrawler):
             url_hash=self.hash(metadata.url),
         )
 
-    def _fetch_metadata(self, page) -> Iterable[FactcheckcenterNewsMetadata]:
-        response = requests.get(
-            self.metadata_api + "/articles/report?page=" + str(page),
-            allow_redirects=False,
-        )
+    def _fetch_metadata(self, page) -> Iterable[EttodayNewsMetadata]:
+        response = None
+        now = datetime.now()
+
+        if page == 0:  # if page == 0 then request potal.
+            response = requests.post(
+                self.metadata_api
+                + "/news/news-list-"
+                + f"{now.year}-{now.month}-{now.day}"
+                + "-0.htm",
+                allow_redirects=False,
+            )
+        else:
+            response = requests.post(
+                self.metadata_api + "/show_roll.php",
+                allow_redirects=False,
+                data={
+                    "offset": page,
+                    "tPage": 3,
+                    "tFile": now.strftime("%Y%M%D") + ".xml",
+                    "tOt": 0,
+                    "tSi": 100,
+                    "tAr": 0,
+                },
+            )
+
         response.raise_for_status()
         response.encoding = "utf-8"
-
         soup = bs4.BeautifulSoup(response.text, "html.parser")
-        html = soup.select(
-            "div.moscone-flipped-content div.view-content > div.views-row > div.views-row-inner"
-        )
+        html = None
+        if page == 0:
+            html = soup.select("div.part_list_2 > h3")
+        else:
+            html = soup.select("h3")
 
         if not html:
             raise ValueError(f'Invalid response: {response}, missing "a" field.')
 
-        parsed_news = list(map(self._parse_factcheckcenter_api_response, html))
+        parsed_news = list(map(self._parse_ettoday_api_response, html))
         return parsed_news
 
-    def _parse_factcheckcenter_api_response(
-        self, html: bs4.element.Tag
-    ) -> FactcheckcenterNewsMetadata:
-        publish_time = parse(
-            html.find("div", {"class": "post-date"}).string.replace("發布日期：", "")
-        )
-        category = html.find("div", {"class": "attr-tag"}).a.string
-        url = html.find("div", {"class": "entity-list-img"}).a["href"]
+    def _parse_ettoday_api_response(self, html: bs4.element.Tag) -> EttodayNewsMetadata:
+        publish_time = parse(html.find("span", {"class": "date"}).string)
+        category = html.find("em", {"class": "tag"}).string
+        url = html.a["href"]
 
         if not publish_time:
             raise ValueError("New's [publish time] is not found.")
@@ -124,8 +151,8 @@ class FactcheckcenterCrawler(BaseCrawler):
         if not url:
             raise ValueError("New's [url] is not found.")
 
-        return FactcheckcenterNewsMetadata(
+        return EttodayNewsMetadata(
             publish_time,
             category,
-            url=self.metadata_api + url,
+            url,
         )
