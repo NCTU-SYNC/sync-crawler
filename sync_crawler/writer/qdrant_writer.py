bad754418d1b5c69a0f79d33a9d31b436d5d133f
David Chiu <david20571015@gmail.com>
2025-03-08 22:42:55 +0800

    feat: enhance database configuration

--- sync_crawler/writer/qdrant_writer.py
+++ sync_crawler/writer/qdrant_writer.py
@@ -3,48 +3,69 @@ from typing import override
 from llama_index.core import Document, VectorStoreIndex
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.qdrant import QdrantVectorStore
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from qdrant_client import QdrantClient
 
 from sync_crawler.models import News
 from sync_crawler.writer.base_writer import BaseWriter
 
 
+class QdrantConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env", ".env.prod"),
+        env_file_encoding="utf-8",
+        env_prefix="QDRANT_",
+        case_sensitive=True,
+        extra="ignore",
+    )
+
+    host: str = Field(
+        "localhost",
+        description="Host of QDrant server.",
+        validation_alias="HOST",
+    )
+    port: int = Field(
+        8000,
+        description="Port of QDrant server.",
+        validation_alias="PORT",
+    )
+    collection: str = Field(
+        "news",
+        description="Name of collection.",
+        validation_alias="COLLECTION",
+    )
+    embedding_model: str = Field(
+        "sentence-transformers/distiluse-base-multilingual-cased-v1",
+        description="Name of embedding model. All available models can be found [here](https://huggingface.co/models?language=zh)",
+    )
+
+
 class QDrantDBWriter(BaseWriter):
     def __init__(
         self,
-        host: str = "localhost",
-        port: int = 8000,
-        token: str = "",
-        collection: str = "news",
-        embedding_model: str = "sentence-transformers/distiluse-base-multilingual-cased-v1",
+        config: QdrantConfig,
         in_memory: bool = False,
     ):
         """Initialize QDrantDBWriter.
 
         Args:
-            host: Host of QDrant server.
-            port: Port of QDrant server.
-            token: Token for authentication.
-            collection: Name of collection.
-            embedding_model: Name of embedding model. All available
-                models can be found [here](https://huggingface.co/models?language=zh)
+            config: Configuration for QDrant database.
             in_memory: Whether to use an in-memory database, usually for testing and development.
-                If True, `host` and `port` will be ignored.
         """
         if in_memory:
             self._client = QdrantClient(":memory:")
         else:
             self._client = QdrantClient(
-                host=host,
-                port=port,
-                # api_key=token,
+                host=config.host,
+                port=config.port,
             )
 
         self._index = VectorStoreIndex.from_vector_store(
             vector_store=QdrantVectorStore(
-                client=self._client, collection_name=collection
+                client=self._client, collection_name=config.collection
             ),
-            embed_model=HuggingFaceEmbedding(model_name=embedding_model),
+            embed_model=HuggingFaceEmbedding(model_name=config.embedding_model),
         )
 
     @staticmethod
