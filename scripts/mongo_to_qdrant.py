0ba7a5998f5c6733f1a9902b842bdef7f4f16af1
David Chiu <david20571015@gmail.com>
2025-03-09 01:49:40 +0800

    feat(mongo_to_qdrant.py): implement migration script from MongoDB to QDrant

---
+++ scripts/mongo_to_qdrant.py
@@ -0,0 +1,75 @@
+import argparse
+import os
+import tomllib
+
+from llama_index.core import Document
+from llama_index.readers.mongodb import SimpleMongoReader
+from tqdm.auto import tqdm
+
+from sync_crawler.writer import QDrantDBWriter, WriterConfig
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        type=str,
+        default=os.path.join("configs", "config.toml"),
+        help="Path to the config file.",
+    )
+    return parser.parse_args()
+
+
+def load_config(config_path: str):
+    try:
+        with open(config_path, "rb") as config_file:
+            config = tomllib.load(config_file)
+    except FileNotFoundError as e:
+        print("Config file not found: %s", e)
+        raise
+    except Exception as e:
+        print("Error loading config file: %s", e)
+        raise
+
+    return WriterConfig.model_validate(config)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    config = load_config(args.config)
+
+    mongo_reader = SimpleMongoReader(uri=config.mongo.url)
+
+    n_docs = mongo_reader.client[config.mongo.database][
+        config.mongo.collection
+    ].count_documents({})
+    print(f"Total documents to migrate: {n_docs}")
+
+    qdrant_writer = QDrantDBWriter(config.qdrant)
+
+    docs = mongo_reader.lazy_load_data(
+        db_name=config.mongo.database,
+        collection_name=config.mongo.collection,
+        field_names=["content"],
+        metadata_names=[
+            "_id",
+            "title",
+            "category",
+            "modified_date",
+        ],
+    )
+
+    for doc in tqdm(docs, total=n_docs, dynamic_ncols=True):
+        qdrant_doc = Document(
+            id_=str(doc.metadata["_id"]),
+            text_resource=doc.text,
+            metadata={
+                "title": doc.metadata["title"],
+                "category": doc.metadata["category"],
+                "modified_date": doc.metadata["modified_date"].timestamp(),
+            },
+            excluded_embed_metadata_keys=QDrantDBWriter.excluded_metadata_keys,
+            excluded_llm_metadata_keys=QDrantDBWriter.excluded_metadata_keys,
+        )
+
+        qdrant_writer._index.insert_nodes([qdrant_doc])
