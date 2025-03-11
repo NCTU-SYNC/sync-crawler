da5040a7e9bd012188788b49b8e87efc53652305
David Chiu <david20571015@gmail.com>
2025-03-12 02:24:22 +0800

    feat(mongo_to_qdrant.py): enhance MongoReader with custom field extractors and improve data loading process docs(README.md): update installation instructions to include optional migration dependencies

--- scripts/mongo_to_qdrant.py
+++ scripts/mongo_to_qdrant.py
@@ -2,12 +2,88 @@ import argparse
 import logging
 import os
 import tomllib
+from collections.abc import Callable, Iterable
 
 from llama_index.core import Document
 from llama_index.readers.mongodb import SimpleMongoReader
 from tqdm.auto import tqdm
 
 from sync_crawler.writer import QDrantDBWriter, WriterConfig
+
+
+class MongoReader(SimpleMongoReader):
+    """Add a custom `field_extractors`.
+
+    Check the [pull request](https://github.com/run-llama/llama_index/pull/18063) for more details.
+
+    """
+
+    def lazy_load_data(
+        self,
+        db_name: str,
+        collection_name: str,
+        field_names: list[str] = ["text"],
+        separator: str = "",
+        query_dict: dict | None = None,
+        max_docs: int = 0,
+        metadata_names: list[str] | None = None,
+        field_extractors: dict[str, Callable[..., str]] | None = None,
+    ) -> Iterable[Document]:
+        """Load data from the input directory.
+
+        Args:
+            db_name (str): name of the database.
+            collection_name (str): name of the collection.
+            field_names(List[str]): names of the fields to be concatenated.
+                Defaults to ["text"]
+            separator (str): separator to be used between fields.
+                Defaults to ""
+            query_dict (Optional[Dict]): query to filter documents. Read more
+            at [official docs](https://www.mongodb.com/docs/manual/reference/method/db.collection.find/#std-label-method-find-query)
+                Defaults to None
+            max_docs (int): maximum number of documents to load.
+                Defaults to 0 (no limit)
+            metadata_names (Optional[List[str]]): names of the fields to be added
+                to the metadata attribute of the Document. Defaults to None
+            field_extractors (Optional[Dict[str, Callable[..., str]]]): dictionary
+                containing field name and a function to extract text from the field.
+                The default extractor function is `str`. Defaults to None.
+
+        Returns:
+            Iterable[Document]: A list of documents.
+        """
+        db = self.client[db_name]
+        cursor = db[collection_name].find(
+            filter=query_dict or {},
+            limit=max_docs,
+            projection={name: 1 for name in field_names + (metadata_names or [])},
+        )
+
+        field_extractors = field_extractors or {}
+
+        for item in cursor:
+            try:
+                texts = [
+                    field_extractors.get(name, str)(item[name]) for name in field_names
+                ]
+            except KeyError as err:
+                raise ValueError(
+                    f"{err.args[0]} field not found in Mongo document."
+                ) from err
+
+            text = separator.join(texts)
+
+            if metadata_names is None:
+                yield Document(text=text, id_=str(item["_id"]))
+            else:
+                try:
+                    metadata = {name: item.get(name) for name in metadata_names}
+                    metadata["collection"] = collection_name
+                except KeyError as err:
+                    raise ValueError(
+                        f"{err.args[0]} field not found in Mongo document."
+                    ) from err
+                yield Document(text=text, id_=str(item["_id"]), metadata=metadata)
 
 
 def parse_args():
@@ -41,7 +117,7 @@ if __name__ == "__main__":
     args = parse_args()
     config = load_config(args.config)
 
-    mongo_reader = SimpleMongoReader(uri=config.mongo.url)
+    mongo_reader = MongoReader(uri=config.mongo.url)
 
     n_docs = mongo_reader.client[config.mongo.database][
         config.mongo.collection
@@ -54,6 +130,9 @@ if __name__ == "__main__":
         db_name=config.mongo.database,
         collection_name=config.mongo.collection,
         field_names=["content"],
+        field_extractors={
+            "content": lambda x: "".join(x),
+        },
         metadata_names=[
             "_id",
             "title",
@@ -64,9 +143,9 @@ if __name__ == "__main__":
 
     for doc in tqdm(docs, total=n_docs, dynamic_ncols=True):
         qdrant_doc = Document(
-            id_=str(doc.metadata["_id"]),
-            text_resource=doc.text,
+            text=doc.text,
             metadata={
+                "mongo_id": str(doc.metadata["_id"]),
                 "title": doc.metadata["title"],
                 "category": doc.metadata["category"],
                 "modified_date": doc.metadata["modified_date"].timestamp(),
